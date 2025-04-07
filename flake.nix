634c74b6e0ad077b6f25a63b7c663f4b90daf978
neetoons <neet.toons@gmail.com>
2025-04-07 06:55:48 -0400

    remove useless code

--- flake.nix
+++ flake.nix
@@ -1,22 +1,16 @@
 {
-  description = "NixOS configuration";
+  description = "Nit's NixOS configuration";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs,  home-manager, ... } @ inputs:
+  outputs = {  nixpkgs,  home-manager, ... }@inputs:
   let 
-  system = "x86_64-linux";
-  localSystem = {system = "x86_64-linux";};
-  pkgs = import nixpkgs {
-      inherit system;
-      config = {
-          allowUnfree = true;
-        };
-    };
-    in 
+    system = "x86_64-linux";
+    localSystem = {system = "x86_64-linux";};
+  in 
   {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
